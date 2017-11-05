aeeb52554cc250914e76bdb19cef993663958a8e
Richard Maciel <richardmaciel@gmail.com>
2017-11-05 01:14:01 -0200

    Implemented native method and refactored RunSuCmdBg

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,16 +1,60 @@
 #include <jni.h>
 #include <string>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <android/log.h>
 
-extern "C"
-JNIEXPORT jstring
+#define SU_PATH "/system/xbin/su"
+#define SU_CMD "su"
+#define SU_FLAG "-c"
+#define CP_CMD "/system/bin/cp"
 
-JNICALL
-Java_com_example_richard_fetchpvtdata_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+#define TAG "NativeFetchPvtData"
+
+// Effectively exec the process which will execute su command
+static void do_child_exec(const char *pvt_path, const char *target_path) {
+    // Child do an execl on su to read file data
+    if(execl(SU_PATH, SU_CMD, SU_FLAG, CP_CMD, pvt_path, target_path, (char *)NULL)) {
+        __android_log_print(ANDROID_LOG_ERROR, TAG, "execl call failed!");
+        exit(1); // if exec failed, process exit status must be non-zero
+    }
 }
+
+// Fork a child, then call the function responsible for setting up
+// child's process and exec'ing the su (and associated copy) command
+// Child's return value will be fed directly to the parent java native method.
+static jboolean internal_suCopyFile(const char *pvt_path, const char *target_path) {
+    pid_t fork_ret;
+    pid_t wait_ret;
+    int wstatus;
+
+    jboolean rv = JNI_FALSE;
+
+    fork_ret = fork();
+    if (!fork_ret) {
+        do_child_exec(pvt_path, target_path);
+    }
+
+    // Parent process gets child's pid
+    // Wait for the child return check its status.
+    wait_ret = wait(&wstatus);
+    if (wait_ret >= 0 && WIFEXITED(wstatus)) {
+        if (!WEXITSTATUS(wstatus)) {
+            rv = JNI_TRUE;
+            goto out;
+        } else {
+            __android_log_print(ANDROID_LOG_ERROR, TAG, "Error executing su call");
+            goto out;
+        }
+    }
+    __android_log_print(ANDROID_LOG_ERROR, TAG, "Wait call error!");
+
+    out:
+    return rv;
+}
+
 extern "C"
 JNIEXPORT jboolean JNICALL
 Java_com_example_richard_fetchpvtdata_utils_SuCmdNative_nativeSuCopyFile(
@@ -18,10 +62,10 @@ Java_com_example_richard_fetchpvtdata_utils_SuCmdNative_nativeSuCopyFile(
     const char *pvtPath = env->GetStringUTFChars(pvtPath_, 0);
     const char *targetPath = env->GetStringUTFChars(targetPath_, 0);
 
-
+    jboolean rv = internal_suCopyFile(pvtPath, targetPath);
 
     env->ReleaseStringUTFChars(pvtPath_, pvtPath);
     env->ReleaseStringUTFChars(targetPath_, targetPath);
 
-    return 0;
+    return rv;
 }
--- app/src/main/java/com/example/richard/fetchpvtdata/MainActivity.java
+++ app/src/main/java/com/example/richard/fetchpvtdata/MainActivity.java
@@ -18,63 +18,9 @@ import com.example.richard.fetchpvtdata.utils.SuCmdFactory;
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
-    RunSuCmdBg runSuCmdBg;
-
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
-    }
-
-    /**
-     * Created this AsyncTask in order to do execute the copy asynchronously with the
-     * main thread, since the file could take some minutes to copy.
-     */
-    // TODO: Change class to a normal one, passing App context as constructor parameter.
-    private class RunSuCmdBg extends AsyncTask<SuCmd, Void, Boolean> {
-        SuCmd suCmd;
-        Context appContext;
-        public RunSuCmdBg(Context c) {
-            appContext = c.getApplicationContext();
-        }
-
-        @Override
-        protected Boolean doInBackground(SuCmd... suCmds) {
-
-            //BuildConfig.DEBUG(suCmds.length == 1);
-            suCmd = suCmds[0];
-            return suCmd.suCopyFile();
-        }
-
-        @Override
-        protected void onPostExecute(Boolean wasSuccessful) {
-            String text, title;
-            if (wasSuccessful) {
-                text = appContext.getString(R.string.copy_success);
-                title = String.format(
-                        appContext.getString(R.string.copy_notification_title_success),
-                        suCmd.getTargetFile().getName());
-            } else {
-                text = appContext.getString(R.string.copy_fail);
-                title = String.format(
-                        appContext.getString(R.string.copy_notification_title_fail),
-                        suCmd.getTargetFile().getName());
-            }
-
-
-            notifyUser(title, text);
-            Log.d(Constants.TAG, "onPostExecute: Ended file copy. Success: " + wasSuccessful);
-        }
-    }
-
-    protected void notifyUser(String title, String notificationText) {
-        Notification.Builder noteBuilder = new Notification.Builder(this);
-        noteBuilder.setSmallIcon(R.drawable.ic_launcher_background);
-        noteBuilder.setContentTitle(title);
-        noteBuilder.setContentText(notificationText);
-
-        NotificationManager notificationManager =
-                (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(1, noteBuilder.build());
     }
 
     @Override
@@ -82,13 +28,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        runSuCmdBg = new RunSuCmdBg(this);
+
 
         // Listener for the Java button
         Button b = findViewById(R.id.java_copy);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                RunSuCmdBg runSuCmdBg = new RunSuCmdBg(getApplicationContext());
                 File srcFile = new File(
                         "/data/data/com.android.messaging/databases/bugle_db");
                 File targetFile = new File(getExternalFilesDir(null),
@@ -106,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
         native_b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                RunSuCmdBg runSuCmdBg = new RunSuCmdBg(getApplicationContext());
                 File srcFile = new File(
                         "/data/data/com.android.keychain/databases/grants.db");
                 File targetFile = new File(getExternalFilesDir(null),
@@ -118,15 +66,5 @@ public class MainActivity extends AppCompatActivity {
                 runSuCmdBg.execute(suCmd);
             }
         });
-
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
     }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 }
